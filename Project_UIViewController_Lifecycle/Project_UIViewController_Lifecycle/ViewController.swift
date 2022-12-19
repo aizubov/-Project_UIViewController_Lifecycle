c93e695436582d1f3a3e25290bff994fe6814539
Alex Zubov <alexanderzubov@icloud.com>
2022-12-19 16:25:02 +0100

    initial_commit_dev

---
+++ Project_UIViewController_Lifecycle/Project_UIViewController_Lifecycle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project_UIViewController_Lifecycle
+//
+//  Created by user226947 on 12/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
